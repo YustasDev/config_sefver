9fb2f882335c7bf862580984d9b5ba32e2b47817
vitaly.gusev <vitaly.gusev@progforce.com>
2024-03-15 18:12:02 +0300

    Initial commit

---
+++ src/main/java/goosseff/site/config_sefver/ConfigSefverApplication.java
@@ -0,0 +1,13 @@
+package goosseff.site.config_sefver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSefverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigSefverApplication.class, args);
+    }
+
+}
